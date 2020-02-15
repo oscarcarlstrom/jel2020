4dc57d128be78c7a14c47a58122220d965d23898
Shahrukh Khan <shahrukh.khan@bouvet.no>
2020-02-15 21:43:59 +0100

    styling popup

--- src/MapMarkers.js
+++ src/MapMarkers.js
@@ -1,52 +1,100 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Marker, Popup } from 'react-leaflet';
-import Icons from './Icons';
+import React from "react";
+import styled from "styled-components";
+import { Marker, Popup } from "react-leaflet";
+import Icons from "./Icons";
 
-const PopContent = styled(Popup)`
-  padding: 12px;
+const PopContent = styled(Popup)``;
+
+const PopContentWrapper = styled.div``;
+
+const PopTitleWrapper = styled.div`
+  font-size: 3.5em;
+  text-decoration: underline;
 `;
 
-const Line = styled.p`
-  margin: 0 !important;
-  margin-bottom: 8px !important;
-`;
+const Line = styled.p``;
 
 const MapMarker = ({ device, position }) => {
   let icon = Icons.greenIcon;
 
-  if(device.BUTTON === '1') {
+  if (device.BUTTON === "1") {
     icon = Icons.redIcon;
   }
-  
+
   if (device.FLIP === "UPSIDE_DOWN") {
     icon = Icons.yellowIcon;
   }
-  
+
   return (
     <Marker position={device.location} icon={icon}>
       <PopContent>
-        <Line>Temperatur: {device.TEMP}</Line>
-        <Line>Fuktighet: {device.HUMID}</Line>
-        <Line>Lufttrykk: {device.AIR_PRESS}</Line>
-        <Line>Luftkvalitet: {device.AIR_QUAL}</Line>
+        <PopTitleWrapper>{device.name} Info</PopTitleWrapper>
+        <PopContentWrapper>
+          <div class="ui center aligned two column grid">
+            <div class="row">
+              <div class="column">
+                <Line>
+                  <div class="ui statistics">
+                    <div class="ui statistic">
+                      <div class="value"> {device.TEMP}</div>
+                      <div class="label">Temperatur</div>
+                    </div>
+                  </div>
+                </Line>
+              </div>
+              <div class="column">
+                <Line>
+                  <div class="ui statistics">
+                    <div class="ui statistic">
+                      <div class="value"> {device.HUMID}</div>
+                      <div class="label">Fuktighet</div>
+                    </div>
+                  </div>
+                </Line>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <Line>
+                  <div class="ui statistics">
+                    <div class="ui statistic">
+                      <div class="value"> {device.AIR_PRESS}</div>
+                      <div class="label">Lufttrykk</div>
+                    </div>
+                  </div>
+                </Line>
+              </div>
+              <div class="column">
+                <Line>
+                  <div class="ui statistics">
+                    <div class="ui statistic">
+                      <div class="value"> {device.AIR_QUAL}</div>
+                      <div class="label">Luftkvalitet</div>
+                    </div>
+                  </div>
+                </Line>
+              </div>
+            </div>
+          </div>
+        </PopContentWrapper>
       </PopContent>
     </Marker>
-  )
-}
-
-const MapMarkers = ({ data, position }) => {
-  console.log('MapMarkers got props', data);
-
-  const markers = Object.values(data).map((device, index) => (
-    <MapMarker key={`marker-${index}`} device={device} position={position} index={index} />
-  ));
-  
-  return (
-    <div>
-      { markers }
-    </div>
-  )
+  );
 };
 
-export default MapMarkers
+const MapMarkers = ({ data, position }) => {
+  console.log("MapMarkers got props", data);
+
+  const markers = Object.values(data).map((device, index) => (
+    <MapMarker
+      key={`marker-${index}`}
+      device={device}
+      position={position}
+      index={index}
+    />
+  ));
+
+  return <div>{markers}</div>;
+};
+
+export default MapMarkers;
