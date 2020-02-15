cbaeb942a9330a4b642df87de1220047723bc26e
Martine Gran <martine.gran@bouvet.no>
2020-02-15 12:37:19 +0100

    add sidebar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MyMap from './Map'
 import './App.css';
 import Dashboard from './Dashboard/Dashboard';
+import Sidebar from "./sidebar";
 
 function App() {
   return (
     <div className="App">
       <MyMap />
+      <Sidebar />
       <Dashboard></Dashboard>
       <footer></footer>
     </div>
