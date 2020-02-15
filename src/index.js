7f9aa8b01dd542f78841ef5940d28fdb61752639
Stian Westvig <stian.westvig@bouvet.no>
2020-02-15 12:56:59 +0100

    Add websocket

--- src/index.js
+++ src/index.js
@@ -4,4 +4,32 @@ import './index.css';
 import '../node_modules/leaflet/dist/leaflet.css'
 import App from './App';
 
+let socket = new WebSocket('ws://10.168.77.182:1880/ws/khan')
+
+socket.onopen = function(e) {
+  console.log('[open] Connection established')
+  socket.send('Client is connected')
+}
+
+socket.onmessage = function(event) {
+  console.log('event', event)
+  console.log(`[message] Data received from server: ${event.data}`)
+  socket.send('Client got data')
+}
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    console.log('[close] Connection died')
+    socket.send('Client disconnected')
+  }
+};
+
+socket.onerror = function(error) {
+  console.eroor(`[ws] ${error.message}`)
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
